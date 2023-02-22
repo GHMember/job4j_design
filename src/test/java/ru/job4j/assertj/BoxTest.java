61e77b17349446676e8559afa8781197d9586667
AlexAutoTesting <zaehor@gmail.com>
2023-02-22 16:23:22 +0300

    Утверждения с примитивными типами [#504883]

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -12,21 +12,24 @@ class BoxTest {
         String name = box.whatsThis();
         assertThat(name).isEqualTo("Sphere").isNotNull();
     }
+
     @Test
-    void NumberOfVerticesIsLessThanZeroAndIsGreaterThanMinusTwo() {
+    void getNumberOfVertices() {
         Box box = new Box(2, 10);
         int number = box.getNumberOfVertices();
         assertThat(number).isLessThan(0).
                 isGreaterThan(-2);
     }
+
     @Test
-    void DoseBoxExist() {
+    void isExist() {
         Box box = new Box(0, 10);
         boolean existence = box.isExist();
         assertThat(existence).isTrue().isNotEqualTo(false);
     }
+
     @Test
-    void Area() {
+    void getArea() {
         Box box = new Box(0, 10);
         double area = box.getArea();
         assertThat(area).isEqualTo(1256.6d, withPrecision(0.1d)).isGreaterThan(1256.6d);
