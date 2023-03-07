ff93cd0d8e6fb1b06b6242d787661587be0b888b
AlexAutoTesting <zaehor@gmail.com>
2023-03-07 14:59:48 +0300

    Правки к заданию 5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,13 +1,18 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    int sizeIn;
-    int sizeOut;
+    private int sizeIn;
+    private int sizeOut;
 
     public T poll() {
         if (sizeOut == 0) {
+            if (sizeIn == 0) {
+                throw new NoSuchElementException();
+            }
             for (; sizeIn > 0; sizeIn--) {
                 out.push(in.pop());
                 sizeOut++;
