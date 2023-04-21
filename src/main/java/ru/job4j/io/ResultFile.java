140a365d1648f3386028059ed5261cb6e3f7c50b
AlexAutoTesting <zaehor@gmail.com>
2023-04-21 14:51:09 +0300

    Правки к заданию 2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,13 +1,14 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
             out.write("Hello, world!".getBytes());
             out.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -18,7 +19,7 @@ public class ResultFile {
             multipleOut.write(System.lineSeparator().getBytes());
             multipleOut.write(("10 * 10 = " + 10 * 10).getBytes());
             multipleOut.write(System.lineSeparator().getBytes());
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
