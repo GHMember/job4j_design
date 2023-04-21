8123ef9bde78f1a9de7f3a8a745b72b2404637d8
AlexAutoTesting <zaehor@gmail.com>
2023-04-21 17:50:22 +0300

    Учебные примеры из темы IO

---
+++ src/main/java/ru/job4j/io/PrintUsage.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class PrintUsage {
+
+    public static void main(String[] args) {
+        try (PrintStream stream = new PrintStream("data/print.txt"); PrintWriter writer = new PrintWriter("data/write.txt")) {
+            stream.println("Из PrintStream в FileOutputStream");
+            stream.write("Новая строка".getBytes());
+            writer.println("Из PrintStream в FileOutputStream");
+            writer.write("Новая строка");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
