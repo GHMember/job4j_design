7e5bde4411b34d2a0abaf512d4e73e5dfd795c81
AlexAutoTesting <zaehor@gmail.com>
2023-04-25 16:31:40 +0300

    6. Читаем файл конфигурации [#858]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConfigTest {
+
+    @Test
+    void whenPairWithoutComment() {
+        String path = "data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("key1")).isEqualTo("value1=1");
+        assertThat(config.value("key2")).isEqualTo("value2=");
+    }
+
+    @Test
+    void whenPairWithComment() {
+        String path = "data/app_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("key1")).isEqualTo("value1=1");
+        assertThat(config.value("key2")).isEqualTo("value2=");
+    }
+
+    @Test
+    void whenPairWithBlankLines() {
+        String path = "data/app_with_blank_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect")).isEqualTo("org.hibernate.dialect.PostgreSQLDialect");
+        assertThat(config.value("key1")).isEqualTo("value1=1");
+        assertThat(config.value("key2")).isEqualTo("value2=");
+    }
+
+    @Test
+    void whenPairStartWithEqualSign() {
+        String path = "data/start_with_=.txt";
+        Config config = new Config(path);
+        Assertions.assertThrows(IllegalArgumentException.class, config::load);
+    }
+
+    @Test
+    void whenPairEndWithEqualSign() {
+        String path = "data/end_with_=.txt";
+        Config config = new Config(path);
+        Assertions.assertThrows(IllegalArgumentException.class, config::load);
+    }
+
+    @Test
+    void whenPairHasOnlyEqualSign() {
+        String path = "data/only_=.txt";
+        Config config = new Config(path);
+        Assertions.assertThrows(IllegalArgumentException.class, config::load);
+    }
+}
