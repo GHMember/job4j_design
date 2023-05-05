fd7044951d386ddb8d62c959ed4a98aa90dbf90b
AlexAutoTesting <zaehor@gmail.com>
2023-05-05 22:18:40 +0300

    Правки к заданию 12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,26 +1,34 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        validation(args);
+        Path start = Paths.get(".");
+        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    }
+
+    private static void validation(String[] args) {
         if (args.length != 2) {
             throw new IllegalArgumentException("Wrong number of arguments. Expected 2.");
         }
-        if (!Objects.equals(args[0], ".")) {
-            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        File file = new File("c:\\");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
-        if (!Objects.equals(args[1], ".js")) {
-            throw new IllegalArgumentException("File format must be \".js\"");
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        if (!args[1].matches(".\\S+")) {
+            throw new IllegalArgumentException("Wrong file format");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
