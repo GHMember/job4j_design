ede82caff5e18f67306ab48bbe22c2f822a8d9cb
AlexAutoTesting <zaehor@gmail.com>
2023-06-19 11:49:28 +0300

    0. JDBC [#6863]

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,24 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Config connectionInfo = new Config("data/app.properties");
+        connectionInfo.load();
+        Class.forName(connectionInfo.value("driver"));
+        String url = connectionInfo.value("url");
+        String login = connectionInfo.value("login");
+        String password = connectionInfo.value("password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
