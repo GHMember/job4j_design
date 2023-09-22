5fd074ca1e0752249a173ddee8e2b47267d26499
AlexAutoTesting <zaehor@gmail.com>
2023-09-22 11:13:47 +0300

    Правки к заданию 2. Принципы Kiss, Dry и Yagni [#238813]

--- src/main/java/ru/job4j/kiss/Fool.java
+++ src/main/java/ru/job4j/kiss/Fool.java
@@ -3,6 +3,16 @@ package ru.job4j.kiss;
 import java.util.Scanner;
 
 public class Fool {
+    private int startAt = 1;
+
+    public int getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(int startAt) {
+        this.startAt = startAt;
+    }
+
     public String getResult(int startAt) {
         String result;
         if (startAt % 3 == 0 && startAt % 5 == 0) {
@@ -17,20 +27,23 @@ public class Fool {
         return result;
     }
 
+    public void check(String answer) {
+        if (!getResult(getStartAt()).equals(answer)) {
+            System.out.println("Ошибка. Начинай снова.");
+            setStartAt(0);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Игра FizzBuzz.");
         Fool fool = new Fool();
-        int startAt = 1;
         var io = new Scanner(System.in);
-        while (startAt < 100) {
-            System.out.println(fool.getResult(startAt));
-            startAt++;
+        while (fool.getStartAt() < 100) {
+            System.out.println(fool.getResult(fool.getStartAt()));
+            fool.setStartAt(fool.getStartAt() + 1);
             var answer = io.nextLine();
-            if (!fool.getResult(startAt).equals(answer)) {
-                System.out.println("Ошибка. Начинай снова.");
-                startAt = 0;
-            }
-            startAt++;
+            fool.check(answer);
+            fool.setStartAt(fool.getStartAt() + 1);
         }
     }
 }
---
+++ src/test/java/ru/job4j/kiss/FoolTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.kiss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FoolTest {
+
+    @Test
+    void whenNumberIsMultiplesOfThreeAndMultiplesOfFive() {
+        Fool fool = new Fool();
+        int startAt = 15;
+        String result = fool.getResult(startAt);
+        assertThat(result).isNotEqualTo("15");
+        assertThat(result).isEqualTo("FizzBuzz");
+    }
+
+    @Test
+    void whenNumberIsMultiplesOfThree() {
+        Fool fool = new Fool();
+        int startAt = 6;
+        String result = fool.getResult(startAt);
+        assertThat(result).isNotEqualTo("6");
+        assertThat(result).isEqualTo("Fizz");
+    }
+
+    @Test
+    void whenNumberIsMultiplesOfFive() {
+        Fool fool = new Fool();
+        int startAt = 10;
+        String result = fool.getResult(startAt);
+        assertThat(result).isNotEqualTo("10");
+        assertThat(result).isEqualTo("Buzz");
+    }
+
+    @Test
+    void whenWrongUserAnswerThenGameRestart() {
+        Fool fool = new Fool();
+        String userAnswer = "3";
+        fool.check(userAnswer);
+        assertThat(fool.getStartAt()).isNotEqualTo(3);
+        assertThat(fool.getStartAt()).isEqualTo(0);
+    }
+
+
+}
